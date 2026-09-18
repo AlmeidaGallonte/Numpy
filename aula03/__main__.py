f291eeed1602a8c077ad6fa3a172bd1c6341ce99
AlmeidaGallonte <jamersonalmeida4567@gmail.com>
2026-09-18 11:18:19 -0300

    Update

--- aula03/__main__.py
+++ aula03/__main__.py
@@ -14,9 +14,21 @@ arr[3] = 40               arr[1][1] = 50
 
 import numpy as np
 
-arr = np.arange(1,11,dtype=int)
-print(arr[:])
+'''arr = np.arange(1,11,dtype=int)
+print(arr)'''
 
-arr
+#indexação unidimencionais
 
+'''print(arr[2:5])'''
 
+#indexação bidimencionais
+'''
+arr2 = np.random.randint(1,10,size=(3,3))
+print(arr2)
+print(arr2[0][2])
+print(arr2[2][0:2])'''
+
+arr3 = np.random.randint(1,101,size=(10,10))
+print(arr3)
+print(arr3[2:4,6:9])
+print(arr3[[2,4,5]])
